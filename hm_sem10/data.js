c011f10f586f911b28464efeed688b9b0e530622
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2023-11-27 00:39:06 +0300

    homework_10

---
+++ hm_sem10/data.js
@@ -0,0 +1,71 @@
+// 20231126230559
+// https://randomuser.me/api/
+
+const datainfo = `[
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Mrs",
+        "first": "Bushra",
+        "last": "Vallestad"
+      },
+      "location": {
+        "street": {
+          "number": 5190,
+          "name": "Enerhauggata"
+        },
+        "city": "Sandeid",
+        "state": "Rogaland",
+        "country": "Norway",
+        "postcode": "6532",
+        "coordinates": {
+          "latitude": "42.1507",
+          "longitude": "79.7352"
+        },
+        "timezone": {
+          "offset": "-10:00",
+          "description": "Hawaii"
+        }
+      },
+      "email": "bushra.vallestad@example.com",
+      "login": {
+        "uuid": "2e7182aa-1043-447c-904a-36c10fdf8b3d",
+        "username": "purpleduck471",
+        "password": "douglas",
+        "salt": "drlsDVwd",
+        "md5": "638342356e7d7684321135186f7571bf",
+        "sha1": "1cbdf1b8c43f7c0c8663948289e74c27104cb2bd",
+        "sha256": "3bbd83746963f1513ed9005a614d7c15b7e3cb535a43e8f40ed37e31b2bc2407"
+      },
+      "dob": {
+        "date": "1988-06-17T18:22:43.247Z",
+        "age": 35
+      },
+      "registered": {
+        "date": "2004-01-13T21:05:12.895Z",
+        "age": 19
+      },
+      "phone": "88957285",
+      "cell": "46012081",
+      "id": {
+        "name": "FN",
+        "value": "17068808081"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/women/5.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/5.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/5.jpg"
+      },
+      "nat": "NO"
+    }
+  ],
+  "info": {
+    "seed": "3f731b2df4f99c1b",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
+  ]`;
